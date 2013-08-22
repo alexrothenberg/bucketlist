d00df348de03e021ef9d605cce649d24e9e9beb7
Alex Rothenberg <alex@alexrothenberg.com>
2013-08-22 17:12:23 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BucketList::Application.config.secret_key_base = 'fc61f3ca3db95ac6b1acf5f2bd98c5e7f8394785bad07b725fbafbec90c611ed0aaa4f019afe78642124ea8f53e84ae238d6c08ad3fa5b30028a2051ebcc9feb'
