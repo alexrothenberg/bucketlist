a4ea499a42cc218114c4c9396c5c86976b542b0d
Alex Rothenberg <alex@alexrothenberg.com>
2013-09-02 10:15:15 -0400

    hide my secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BucketList::Application.config.secret_key_base = 'fc61f3ca3db95ac6b1acf5f2bd98c5e7f8394785bad07b725fbafbec90c611ed0aaa4f019afe78642124ea8f53e84ae238d6c08ad3fa5b30028a2051ebcc9feb'
+BucketList::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'fc61f3ca3db95ac6b1acf5f2bd98c5e7f8394785bad07b725fbafbec90c611ed0aaa4f019afe78642124ea8f53e84ae238d6c08ad3fa5b30028a2051ebcc9feb'
